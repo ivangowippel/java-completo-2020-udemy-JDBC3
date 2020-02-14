55476fe00c182313f09bc597dfd954a6f871b8aa
Ivango <ognavi@gmail.com>
2020-02-14 07:30:33 -0300

    Aula 229 - JDBC Inserindo dados do banco MySQL com vários elementos

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import db.DB;
@@ -19,7 +18,7 @@ public class Program {
 		PreparedStatement st = null;
 		try {
 			conn = DB.getConnection();
-
+			/*
 			st = conn.prepareStatement("INSERT INTO seller "
 					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
 					+ "VALUES " + "(?, ?, ?, ?, ?)",
@@ -30,9 +29,14 @@ public class Program {
 			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
 			st.setDouble(4, 3000.0);
 			st.setInt(5, 4);
-
+			*/
+			
+			st = conn.prepareStatement(
+					"insert into department (Name) values ('D1'),('D2')",
+					Statement.RETURN_GENERATED_KEYS);
+			
 			int rowsAffected = st.executeUpdate();
-
+			
 			if (rowsAffected > 0) {
 				ResultSet rs = st.getGeneratedKeys();
 				while (rs.next()) {
@@ -44,8 +48,6 @@ public class Program {
 			}
 
 		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (ParseException e) {
 			e.printStackTrace();
 		} finally {
 			DB.closeStatement(st);
